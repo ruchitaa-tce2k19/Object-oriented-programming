c2b0b4dcdfbcdea563c9be7678853bd056ba3a85
ruchitaa-tce2k19 <53228381+ruchitaa-tce2k19@users.noreply.github.com>
2022-04-30 12:28:13 +0530

    Add files via upload

--- Inheritance/AnimalInheritance.java
+++ Inheritance/AnimalInheritance.java
@@ -1,1 +1,28 @@
+package Inheritance;
+/*
+ * Single inheritance is the simplest type of inheritance in java. 
+ * In this, a class inherits the properties from a single class. 
+ * The class which inherits is called the derived class or child class or 
+ * subclass, while the class from which the derived class inherits is called 
+ * base class or superclass or parent class.*/
+class Animal {	 
+	String name;
+	public void eat() {
+		System.out.println("I eat pedigree");
+	}
+} 
+class Dog extends Animal {	 
+	public void display() {
+		System.out.println("My name is " + name);
+	}
+}
 
+class AnimalInheritance {
+	public static void main(String[] args) {		 
+		Dog labrador = new Dog();		 
+		labrador.name = "Sugar";
+		labrador.display();		 
+		labrador.eat();
+
+	}
+}
